989673ba05ecb0c516deda3f9d076ca8468ff4a7
peiyang wu <peiyangwu37@gmail.com>
2023-03-16 21:32:12 +0100

    added parameter images instance

--- src/main/java/com/example/LibraryEntry.java
+++ src/main/java/com/example/LibraryEntry.java
@@ -4,19 +4,11 @@ import javafx.scene.image.Image;
 
 public class LibraryEntry {
 
-    /**constructor of LibraryEntry:Der Konstruktor nimmt (mindestens) einen String mit der Bild-url und ein Arbitrary
-     Number Argument, das die Features eines Feldes beschreibt, daraus werden im
-     Konstruktor die Sockets erzeugt..*/
 
-    public LibraryEntry( String path, Feature ... features){
-        this.imagePath = path;
-
-
-    }
 
     /** declaration of arrtributes for one Libraryentry
      */
-    // (a) String mit url zur Ablage des Bilds;
+    //(a) String mit url zur Ablage des Bilds;
     String imagePath;
 
 
@@ -30,14 +22,19 @@ public class LibraryEntry {
             this.type = type;
             this.sockets = sockets;
         }
-
     }
 
-    //eine Instanz des Bildes als JavaFX Node (Image).
+    //(c) eine Instanz des Bildes als JavaFX Node (Image).
     Image img;
 
+    /**constructor of LibraryEntry:Der Konstruktor nimmt (mindestens) einen String mit der Bild-url und ein Arbitrary
+     Number Argument, das die Features eines Feldes beschreibt, daraus werden im
+     Konstruktor die Sockets erzeugt..*/
 
-
+    public LibraryEntry( String path, Image img, Feature ... features){
+        this.imagePath = path;
+        this.img = img;
+    }
 
 
     }
--- src/main/java/com/example/TileLibrary.java
+++ src/main/java/com/example/TileLibrary.java
@@ -1,4 +1,6 @@
 package com.example;
+import javafx.scene.image.Image;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,7 +18,7 @@ public class TileLibrary {
     /** creating entries for all possible tiles */
 
     // for tile of image d
-    LibraryEntry d= new LibraryEntry("file:src/field/D.png",
+    LibraryEntry d= new LibraryEntry("file:src/field/D.png", new Image("file:src/field/D.png"),
             new LibraryEntry.Feature("field",0,8,9,10,11),
             new LibraryEntry.Feature("road",1,7),
             new LibraryEntry.Feature("field",2,6),
@@ -26,14 +28,14 @@ public class TileLibrary {
 
 
     // for tile of image h
-    LibraryEntry h= new LibraryEntry("file:src/field/D.png",
+    LibraryEntry h= new LibraryEntry("file:src/field/H.png", new Image("file:src/field/H.png"),
             new LibraryEntry.Feature("field",0,1,2,6,7,8),
             new LibraryEntry.Feature("city",3,4,5),
             new LibraryEntry.Feature("city",9,10,11)
     ){};
 
     // for tile of image j
-    LibraryEntry j = new LibraryEntry("file:src/field/J.png",
+    LibraryEntry j = new LibraryEntry("file:src/field/J.png", new Image("file:src/field/J.png"),
             new LibraryEntry.Feature("field",3,8,9,10,11),
             new LibraryEntry.Feature("city",0,1,2),
             new LibraryEntry.Feature("road",4,7),
@@ -41,20 +43,22 @@ public class TileLibrary {
 
 
     // for tile of image k
-    LibraryEntry k = new LibraryEntry("file:src/field/K.png",new LibraryEntry.Feature("field",0,11),
-            new LibraryEntry.Feature("road",1,10), new LibraryEntry.Feature("field",2,6,7,8,9),
+    LibraryEntry k = new LibraryEntry("file:src/field/K.png", new Image("file:src/field/K.png"),
+            new LibraryEntry.Feature("field",0,11),
+            new LibraryEntry.Feature("road",1,10),
+            new LibraryEntry.Feature("field",2,6,7,8,9),
             new LibraryEntry.Feature("city",3,4,5) ){};
 
 
     // for tile of image v
-    LibraryEntry v = new LibraryEntry("file:src/field/V.png",
+    LibraryEntry v = new LibraryEntry("file:src/field/V.png", new Image("file:src/field/V.png"),
             new LibraryEntry.Feature("field",0,9,
     10,11), new LibraryEntry.Feature("road",1,7),
             new LibraryEntry.Feature("field",2,3,4,5,6)){};
 
 
     // for tile of image w
-    LibraryEntry w =new LibraryEntry("file:src/field/W.png",
+    LibraryEntry w =new LibraryEntry("file:src/field/W.png", new Image("file:src/field/W.png"),
                     new LibraryEntry.Feature("field",0,1,2,3,4,5,6),
                     new LibraryEntry.Feature("road",7,10),
                     new LibraryEntry.Feature("field",8,9)){};
