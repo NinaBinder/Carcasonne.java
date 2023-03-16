7e4dba81f2afa9afdc0cd4926edb9ca1a030b32d
NinaBinder <Nina.Binder@campus.lmu.de>
2023-03-16 20:36:01 +0100

    Begin Library Entry

--- src/main/java/com/example/LibraryEntry.java
+++ src/main/java/com/example/LibraryEntry.java
@@ -1,24 +1,76 @@
-package com.example;
+package  com.example;
 
-import javafx.scene.image.Image;
+        import javafx.scene.image.Image;
 
 public class LibraryEntry {
-    //Enthält die Beschreibung eines Spielfelds, dies wird zur Erzeugen der Tile Objekte genutzt.
-    //ein LibraryEntry enthält:
-    // (a) String mit url zur Ablage des Bilds;
-    String imagePath = new String();
-    //imagePath = "file:src/fields/A.png";
-    // (b) eine Beschreibung der Sockets;
 
-    // (c) eine Instanz des Bildes als JavaFX Node (Image).
-    Image img = new Image("file:src/fields/A.png");
+    String imagePath;
+    Image image;
 
-
-    //Der Konstruktor nimmt (mindestens) einen String mit der Bild-url und ein Arbitrary
-    //Number Argument, das die Features eines Feldes beschreibt, daraus werden im
-    //Konstruktor die Sockets erzeugt.
-
-    public LibraryEntry(){
+    public LibraryEntry( String path, Image image ,Feature ... features){
+        this.imagePath = path;
+        image= path;
 
     }
-}
+
+    //for now saving all the image names paths in an array
+    Image imgA= A.png";
+
+    /*"B.png","C.png",
+            "D.png",
+            "E.png",
+            "F.png",
+            "G.png",
+            "H.png",
+            "I.png",
+            "J.png",
+            "K.png",
+            "L.png",
+            "M.png",
+            "N.png",
+            "O.png",
+            "P.png",
+            "Q.png",
+            "S.png",
+            "T.png",
+            "U.png",
+            "V.png",
+            "W.png",
+            "X.png",
+            "Y.png"};*/
+
+
+    // arrtributes ryentry
+     //Enthält die Beschreibung eines Spielfelds, dies wird zur Erzeugen der Tile Objekte genutzt.
+     //ein LibraryEntry enthält:
+     // (a) String mit url zur Ablage des Bilds;
+
+
+
+     //a des""R.png!,ription of the sockets in the Feature class e.g. new Feature("city", 3,4,5)
+     // new Feature("Fields",1.7)
+     class Feature {
+     String type;
+     int[] sockets;
+
+     public Feature(String type, int ... sockets) {
+     this.type = type;
+     this.sockets = sockets;
+     }
+
+
+     //eine Instanz des Bildes als JavaFX Node (Image).
+
+
+
+
+     /**constructor of LibraryEntry:Der Konstruktor nimmt (mindestens) einen String mit der Bild-url und ein Arbitrary
+     Number Argument, das die Features eines Feldes beschreibt, daraus werden im
+     Konstruktor die Sockets erzeugt..*/
+
+
+
+
+
+}}
+
--- src/main/java/com/example/TileLibrary.java
+++ src/main/java/com/example/TileLibrary.java
@@ -1,8 +1,64 @@
 package com.example;
 
-public class TileLibrary {
-    //Die TileLibrary dient als Klasse als Nachschlagewerk aller möglichen Kacheln.
-    //Sie enthält eine Hashmap, die die Buchstaben Benennung der Felder in der Anleitung
-    //(siehe Seite B1) auf einen LibraryEntry mapt. Sie enthält Methoden um auf die
-    //Einträge über die Buchstaben Benennung oder zufällig zuzugreifen.
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.util.HashMap;
+
+public class TileLibrary{
+    HashMap<String, LibraryEntry> map;
+
+    public TileLibrary(HashMap<String, LibraryEntry> map){
+        this.map= map;
+
+        map.put("K",k);
+        map.put("D",d);
+        map.put("V",v);
+        map.put("W",w);
+        map.put("H",h);
+        map.put("J",j);
+
+    }
+
+    LibraryEntry k = new LibraryEntry("file:src/field/A.png",
+
+            new LibraryEntry.Feature("field",0,11),
+            new LibraryEntry.Feature("road",1,10),
+            new LibraryEntry.Feature("field",2,6,7,8,9),
+            new LibraryEntry.Feature("city",3,4,5)
+    ) {};
+    LibraryEntry d= new LibraryEntry("file:src/field/D.png",
+            new LibraryEntry.Feature("field",0,8,9,10,11),
+            new LibraryEntry.Feature("road",1,7),
+            new LibraryEntry.Feature("field",2,6),
+            new LibraryEntry.Feature("city",3,4,5)
+    ){};
+    LibraryEntry h= new LibraryEntry("file:src/field/D.png",
+            new LibraryEntry.Feature("field",0,1,2,6,7,8),
+            new LibraryEntry.Feature("city",3,4,5),
+            new LibraryEntry.Feature("city",9,10,11)
+    ){};
+
+    // for tile of image v
+    LibraryEntry v = new LibraryEntry("file:src/field/V.png",
+            new LibraryEntry.Feature("field",0,9, 10,11),
+            new LibraryEntry.Feature("road",1,7),
+            new LibraryEntry.Feature("field",2,3,4,5,6)
+    ){};
+
+
+    // for tile of image w
+    LibraryEntry w = new LibraryEntry("file:src/field/W.png",
+            new LibraryEntry.Feature("field",0,1,2,3,4,5,6),
+            new LibraryEntry.Feature("road",7,10),
+            new LibraryEntry.Feature("field",8,9)
+    ){};
+
+
+    // for tile of image j
+    LibraryEntry j = new LibraryEntry("file:src/field/J.png",
+            new LibraryEntry.Feature("field",3,8,9,10,11),
+            new LibraryEntry.Feature("city",0,1,2),
+            new LibraryEntry.Feature("road",4,7),
+            new LibraryEntry.Feature("field",5,6)
+    ){};
 }
