015a1792b55bf6cae2641b50675ca63e1b33fa35
NinaBinder <Nina.Binder@campus.lmu.de>
2023-03-23 19:21:31 +0100

    Tile class updates

--- src/main/java/com/example/Tile.java
+++ src/main/java/com/example/Tile.java
@@ -2,8 +2,6 @@ package com.example;
 
 import javafx.scene.image.Image;
 
-import java.util.Map;
-
 
 //Die Klasse Tile beschreibt die einzelnen Spielkarten, welche in Summe das Spielfeld
 //ergeben.
@@ -15,34 +13,39 @@ import java.util.Map;
 
 public class Tile {
     TileLibrary library= new TileLibrary();
-
-
     int relX;
     int relY;
     int rotation;
-    Image image;
-    Socket[] sockets;
+    boolean gamePiece;
+    LibraryEntry entry;
+
+    public Image getImage(){
+        return entry.img;
+    }
+
+    public Socket[] getSockets(){
+        return entry.sockets;
+    }
 
     // Jede Tile speichert:
     // (a) seine relative Position innerhalb des Spielfelds als x und y ab;
     // (b) die eigene Rotation;
     // (c) das assoziierte Bild;
     // (d) und ein Array, das die Konfiguration der Seiten beschreibt (Socket Array),
-    public Tile(int relX,int relY, int rotation, String entry){
+    public Tile(int relX,int relY, int rotation, String entry, boolean gamePiece){
         this.relX= relX;
         this.relY= relY;
         this.rotation= rotation;
-        library.map.get(entry);
-
+        this.entry= library.map.get(entry);
+        this.gamePiece = gamePiece;
     }
-    Tile originalTile = new Tile(0,0,0,"OG");
-
 
     //placement of a character on this tile
 
 
     //Setter
     /** Set the current rotation of the tile, e.g. "links" */
+    //TODO: rotate sockets as well!!!
     public void setRotation(int rotation) {
         this.rotation = rotation;
     }
@@ -53,16 +56,10 @@ public class Tile {
         return rotation;
     }
 
-
     // method to rotate the file
     public void rotate(){
         //setRotation((getRotation()=="links")?"rechts":"links");
     }
-
-
-    //kann ein
-    //ImageView Node erzeugt werden mit spezifizieter Dimension und richtiger Rotation;
-
 
 }
 
