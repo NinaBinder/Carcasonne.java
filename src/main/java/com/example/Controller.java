0c809b69e5b0a0a2948e2232f81af6fb989cf5ed
peiyang wu <peiyangwu37@gmail.com>
2023-03-25 14:20:07 +0100

    controller merged

--- src/main/java/com/example/Controller.java
+++ src/main/java/com/example/Controller.java
@@ -1,18 +1,24 @@
 package com.example;
+
+import javafx.event.EventTarget;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import javafx.scene.shape.Rectangle;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
 
 public class Controller {
     View view= new View();
-
-    Tile originalTile= new Tile(0,0,0,"OG", false);
-    Board board= new Board();
-    TileLibrary library= new TileLibrary();
+    TileLibrary library = new TileLibrary();
+    ArrayList<Tile> allTiles;
+    Board board= new Board( );
+    Tile ButtonTile;
 
 
     /** deck: An Array List filled with all possible Entries
@@ -21,13 +27,29 @@ public class Controller {
     ArrayList<LibraryEntry> deck= new ArrayList<LibraryEntry>();
 
     public Controller(View view){
-        this.view= view;
+        this.view = view;
+        this.allTiles = new ArrayList<Tile>();
+        // generate original tile and new empty tiles in the tile array
+        allTiles.add(new Tile(-1,-1,0,"EMPTY",false));
+        allTiles.add(new Tile(0,-1,0,"EMPTY",false));
+        allTiles.add(new Tile(1,-1,0,"EMPTY",false));
+        allTiles.add(new Tile(-1,0,0,"EMPTY",false));
+        allTiles.add(new Tile(0,0,0,"OG",false));
+        allTiles.add(new Tile(1,0,0,"EMPTY",false));
+        allTiles.add(new Tile(-1,1,0,"EMPTY",false));
+        allTiles.add(new Tile(0,1,0,"EMPTY",false));
+        allTiles.add(new Tile(1,1,0,"EMPTY",false));
+
         deck.add(library.map.get("A"));
         deck.add(library.map.get("B"));
+
 
         changeDrawButtonImage();
 
     }
+
+
+
 
     public void changeDrawButtonImage(){
 //TODO: nochmal ziehen, falls tile nicht passt
@@ -50,6 +72,58 @@ public class Controller {
         });
     }
 
+    // drag the tile which is shown on the button to the board
+    public void DragandDrop(){
+        //Source of the drag gesture
+        Button source = view.drawCardButton;
+        //react of drag
+        source.setOnMouseClicked(event -> {
+            Dragboard db = source.startDragAndDrop(TransferMode.ANY);
+            ClipboardContent content = new ClipboardContent();
+            System.out.println(view.getButtonImage());
+            content.putImage(view.getButtonImage());
+            db.setContent(content);
+            event.consume();
+        });
+        // target of drag gesture = drop
+
+
+        for(Tile tile: allTiles){
+            Tile target = tile;
+            // accept possible drop
+
+
+        }
+    }
+
+
+    //set the event handler on the rotateRight button
+    public void rotateRight(){
+        view.rotateRight.setOnAction(event-> {
+            //rotate the sockets of the ButtonTile
+            ButtonTile.rotateRight();
+            // the rotation of the ButtonTile will be updated
+            // rotate the image with the new rotation value
+            view.rotate(ButtonTile.getRotation());
+        });
+    }
+
+    //set the event handler on the rotateLeft button
+    public void rotateLeft(){
+        view.rotateLeft.setOnAction(event-> {
+            //rotate the sockets of the ButtonTile
+            ButtonTile.rotateLeft();
+            // the rotation of the ButtonTile will be updated
+            // rotate the image with the new rotation value
+            view.rotate(ButtonTile.getRotation());
+
+        });
+    }
+
+
+
+
+
 
     // get neighbours (north, east, south, west) of a tile
     public Tile getNorthTile(int x, int y){
@@ -70,43 +144,7 @@ public class Controller {
     }
 
 
-    //check whether the field fits on the side of another
-    //TODO: model
-    public Boolean tilesMatch(int x, int y){
-        Tile tile= board.matrix[x][y];
-        Tile north = board.matrix[x][y-1];
-        Tile east= board.matrix[x+1][y];
-        Tile south= board.matrix[x][y+1];
-        Tile west= board.matrix[x-1][y];
 
-        if (tile.getSockets()[0].getComponent()!=north.getSockets()[8].getComponent())
-            return false;
-        if (tile.getSockets()[1].getComponent()!=north.getSockets()[7].getComponent())
-            return false;
-        if (tile.getSockets()[2].getComponent()!=north.getSockets()[6].getComponent())
-            return false;
-
-        if (tile.getSockets()[3].getComponent()!=east.getSockets()[11].getComponent())
-            return false;
-        if (tile.getSockets()[4].getComponent()!=east.getSockets()[10].getComponent())
-            return false;
-        if (tile.getSockets()[5].getComponent()!=east.getSockets()[9].getComponent())
-            return false;
-
-        if (tile.getSockets()[6].getComponent()!=south.getSockets()[2].getComponent())
-            return false;
-        if (tile.getSockets()[7].getComponent()!=south.getSockets()[1].getComponent())
-            return false;
-        if (tile.getSockets()[8].getComponent()!=south.getSockets()[0].getComponent())
-            return false;
-
-        if (tile.getSockets()[11].getComponent()!=west.getSockets()[3].getComponent())
-            return false;
-        if (tile.getSockets()[10].getComponent()!=west.getSockets()[4].getComponent())
-            return false;
-        if (tile.getSockets()[9].getComponent()!=west.getSockets()[5].getComponent())
-            return false;
-        return true;
     }
 
 
