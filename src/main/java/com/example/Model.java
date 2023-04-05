131e774d2f0413b584da71696a1b18066b3770a3
NinaBinder <Nina.Binder@campus.lmu.de>
2023-04-05 11:37:32 +0200

    finished drag and drop, try place tile method, is matching method, small changes in update View/ Board method, changed Library Entries

--- src/main/java/com/example/Model.java
+++ src/main/java/com/example/Model.java
@@ -17,13 +17,15 @@ public class Model {
     private LibraryEntry nextEntry;
     private List<Player> players;
     private int currentPlayerIndex;
-    private Controller controller;
-
+    private Controller controller= new Controller(new View(),this);
+    View view = new View();
 
 
     public Model (){
-
         board = new Board(this);
+        System.out.println(controller.getRotation());
+
+
     }
     public void addEmptyTiles(Tile tile){
 
@@ -53,20 +55,23 @@ public class Model {
 
     public boolean tryPlaceTile(int relX, int relY){
 
-        System.out.println("Rel Position of next Tile: " + relX + "," +relY);
+        //System.out.println("Rel Position of next Tile: " + relX + "," +relY);
         Tile targetTile = getBoard().getRelativeTile(relX, relY);
-        System.out.println("target Tile Entry: " + targetTile.getEntry());
+        //System.out.println("target Tile Entry: " + targetTile.getEntry());
 
         Tile northTile= getNorthTile(targetTile.getRelX(), targetTile.getRelY());
         Tile southTile= getSouthTile(targetTile.getRelX(), targetTile.getRelY());
         Tile eastTile= getEastTile(targetTile.getRelX(), targetTile.getRelY());
         Tile westTile= getWestTile(targetTile.getRelX(), targetTile.getRelY());
+
         //System.out.println(westTile);
 
         String nextEntry = controller.getLibrary().getNameOfEntry(this.nextEntry);
         System.out.println("next Tile Entry: " + nextEntry);
+        //System.out.println("rotatation" + controller.getRotation());
 
-        Tile nextTile= new Tile(relX, relY, targetTile==null ? 0 : targetTile.getRotation(), nextEntry, targetTile==null ? false : targetTile.gamePiece);
+        Tile nextTile= new Tile(relX, relY, targetTile==null ? 0 : 0, nextEntry, targetTile==null ? false : targetTile.gamePiece);
+
 
         //can setTile? is empty & is matching with neighbors?
         if(tileMatch(nextTile,northTile,southTile,eastTile,westTile)){
@@ -82,39 +87,173 @@ public class Model {
         return false;
     }
     /** check if the field fits on the side of another neighbours*/
-    public boolean tileMatch (Tile tile, Tile northTile, Tile southTile, Tile eastTile, Tile westTile){
+    public boolean tileMatch (Tile nextTile, Tile northTile, Tile southTile, Tile eastTile, Tile westTile) {
 
-        if(isEmpty(northTile)&& isEmpty(eastTile) && isEmpty(southTile) && isEmpty(westTile) ){
+        LibraryEntry.Component nextOben = nextTile.getNorthEdge();
+        LibraryEntry.Component nextRechts = nextTile.getEastEdge();
+        LibraryEntry.Component nextUnten = nextTile.getSouthEdge();
+        LibraryEntry.Component nextLinks= nextTile.getWestEdge();
+
+        if (northTile == null && eastTile == null && southTile == null && westTile == null) {
+            return false;
+
+        } else if (!isEmpty(northTile)) {
+            LibraryEntry.Component oben = northTile.getSouthEdge();
+            System.out.println("oben " + oben);
+
+            if (oben == nextOben) {
+                System.out.println("next oben " + nextOben);
+                return true;
+            }
+
+        } else if (!isEmpty(eastTile)) {
+            LibraryEntry.Component rechts = eastTile.getWestEdge();
+            System.out.println("rechts " + rechts);
+
+            if (rechts == nextRechts) {
+                System.out.println("next rechts " + nextRechts);
+                return true;
+            }
+
+        }else if (!isEmpty(southTile)) {
+            LibraryEntry.Component unten = southTile.getNorthEdge();
+            System.out.println("unten " + unten);
+
+            if (unten == nextUnten) {
+                System.out.println("next unten " + nextUnten);
+                return true;
+            }
+
+        }else if (!isEmpty(westTile)) {
+            LibraryEntry.Component links= westTile.getEastEdge();
+            System.out.println("links " + links);
+
+            if (links == nextLinks) {
+                System.out.println("next links " + nextLinks);
+                return true;
+            }
+        }
+
+
+        /*
+
+
+        if(!isEmpty(northTile)){
+
+            if(isEmpty(westTile)){
+                return true;
+            } else if (westTile.getEastEdge() == nextTile.getEastEdge() || westTile.getEastEdge() == LibraryEntry.Component.UNIVERSAL) {
+                return true;
+            }
+            if(isEmpty(eastTile)){
+                return true;
+            } else if (eastTile.getWestEdge() == nextTile.getWestEdge() || eastTile.getWestEdge() == LibraryEntry.Component.UNIVERSAL) {
+                return true;
+            }
+            if(isEmpty(southTile)){
+                return true;
+            } else if (southTile.getNorthEdge()== nextTile.getSouthEdge() || southTile.getNorthEdge() == LibraryEntry.Component.UNIVERSAL) {
+                return true;
+            }
+        }
+        if(!isEmpty(eastTile)){
+            System.out.println(eastTile.getWestEdge());
+
+            if(isEmpty(westTile)){
+                return true;
+            } else if (westTile.getEastEdge() == nextTile.getEastEdge() || westTile.getEastEdge() == LibraryEntry.Component.UNIVERSAL) {
+                return true;
+            }
+            if(isEmpty(northTile)){
+                return true;
+            } else if (northTile.getSouthEdge() == nextTile.getNorthEdge() || northTile.getSouthEdge() == LibraryEntry.Component.UNIVERSAL) {
+                return true;
+            }
+            if(isEmpty(southTile)){
+                return true;
+            } else if (southTile.getNorthEdge()== nextTile.getSouthEdge() || southTile.getNorthEdge() == LibraryEntry.Component.UNIVERSAL) {
+                return true;
+            }
+        }
+        if(!isEmpty(southTile)){
+            System.out.println(southTile.getNorthEdge());
+
+            if(isEmpty(westTile)){
+                return true;
+            } else if (westTile.getEastEdge() == nextTile.getEastEdge() || westTile.getEastEdge() == LibraryEntry.Component.UNIVERSAL) {
+                return true;
+            }
+            if(isEmpty(eastTile)){
+                return true;
+            } else if (eastTile.getWestEdge() == nextTile.getWestEdge() || eastTile.getWestEdge() == LibraryEntry.Component.UNIVERSAL) {
+                return true;
+            }
+            if(isEmpty(northTile)){
+                return true;
+            } else if (northTile.getSouthEdge()== nextTile.getNorthEdge() || northTile.getSouthEdge() == LibraryEntry.Component.UNIVERSAL) {
+                return true;
+            }
+        }
+        if(!isEmpty(westTile)){
+            System.out.println(westTile.getEastEdge());
+
+            if(!isEmpty(northTile)){
+                return true;
+            } else if (northTile.getSouthEdge() == nextTile.getNorthEdge() || northTile.getSouthEdge() == LibraryEntry.Component.UNIVERSAL) {
+                return true;
+            }
+            if(isEmpty(eastTile)){
+                return true;
+            } else if (eastTile.getWestEdge() == nextTile.getWestEdge() || eastTile.getWestEdge() == LibraryEntry.Component.UNIVERSAL) {
+                return true;
+            }
+            if(isEmpty(southTile)){
+                return true;
+            } else if (southTile.getNorthEdge()== nextTile.getSouthEdge() || southTile.getNorthEdge() == LibraryEntry.Component.UNIVERSAL) {
+                return true;
+            }
+        }*/
+        /*
+
+        //is there a tile north?
+        if (northTile != null) {
+            System.out.println("new Tile north:" + tile.getNorthEdge() + "=?" + northTile.getSouthEdge());
+
+            //is northtile at connecting part mathcing or universal?
+            if (northTile.getSouthEdge() == LibraryEntry.Component.UNIVERSAL || tile.getNorthEdge() == northTile.getSouthEdge()) {
+                return true;
+            }
+        }
+
+        if (eastTile != null) {
+            System.out.println("new Tile eastEdge:" + tile.getEastEdge() + "=?" + eastTile.getWestEdge());
+
+            if (eastTile.getWestEdge() == LibraryEntry.Component.UNIVERSAL || tile.getEastEdge() == eastTile.getWestEdge()) {
+                return true;
+            }
+        }
+
+
+        if (westTile != null) {
+            System.out.println("new Tile westEdge:" + tile.getWestEdge() + "=?" + westTile.getEastEdge());
+
+            if (westTile.getEastEdge() == LibraryEntry.Component.UNIVERSAL || tile.getWestEdge() == westTile.getEastEdge()) {
+                return true;
+            }
+        }
+
+
+        if (southTile != null) {
+            System.out.println("new Tile southEdge:" + tile.getSouthEdge() + "=?" + southTile.getNorthEdge());
+
+            if ( southTile.getNorthEdge() == LibraryEntry.Component.UNIVERSAL || tile.getSouthEdge() == southTile.getNorthEdge()) {
+                 return true;
+                }
+            }*/
+
             return false;
         }
 
-        if(northTile!=null ){
-            if( tile.getNorthEdge() != northTile.getSouthEdge() || northTile.getSouthEdge() == LibraryEntry.Component.UNIVERSAL ){
-                System.out.println("new Tile northEdge:" + tile.getNorthEdge() + "=?" + northTile.getSouthEdge());
-                return false;
-            }
-        }
-        if(eastTile!=null){
-            if(tile.getEastEdge() !=  eastTile.getWestEdge() || eastTile.getWestEdge() == LibraryEntry.Component.UNIVERSAL){
-                System.out.println("new Tile eastEdge:" + tile.getEastEdge() + "=?" + eastTile.getWestEdge() );
-                return false;
-            }
-        }
-
-        if(westTile!=null){
-            if(tile.getWestEdge() != westTile.getEastEdge() || westTile.getEastEdge() == LibraryEntry.Component.UNIVERSAL){
-                System.out.println("new Tile westEdge:" + tile.getWestEdge() + "=?" + westTile.getEastEdge() );
-                return false;
-            }
-        }
-        if(southTile!=null){
-            if(tile.getSouthEdge() != southTile.getNorthEdge() || southTile.getNorthEdge() == LibraryEntry.Component.UNIVERSAL){
-                System.out.println("new Tile southEdge:" + tile.getSouthEdge() + "=?" + southTile.getNorthEdge() );
-                return false;
-            }
-        }
-        return true;
-    }
 
     public Tile getNorthTile(int relX, int relY){
         Tile north = getBoard().getRelativeTile(relX,relY-1);
